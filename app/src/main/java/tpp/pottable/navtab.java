1b924db842bad9d243fc9a77dc5b1fd5854004dd
Hu?nh Thi?n Khiêm <hkhhiem@gmail.com>
2018-10-01 07:07:10 +0700

    Code cleanup

--- app/src/main/java/tpp/pottable/navtab.java
+++ app/src/main/java/tpp/pottable/navtab.java
@@ -94,32 +94,6 @@ public class navtab extends AppCompatActivity {
     /*DATABASE INIT*/
     private DatabaseHelper mDBHelper;
     private SQLiteDatabase mDb;
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-    }
 
     public static class database extends Fragment {
 
