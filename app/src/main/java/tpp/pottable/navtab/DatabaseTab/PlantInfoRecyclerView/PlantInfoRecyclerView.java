7415a0e5b9bdb26766b9f869f680976079150a6d
Hu?nh Thi?n Khiêm <hkhhiem@gmail.com>
2018-10-01 20:26:38 +0700

    Temporary backup

--- app/src/main/java/tpp/pottable/navtab/DatabaseTab/PlantInfoRecyclerView/PlantInfoRecyclerView.java
+++ app/src/main/java/tpp/pottable/navtab/DatabaseTab/PlantInfoRecyclerView/PlantInfoRecyclerView.java
@@ -1,10 +1,12 @@
-package tpp.pottable;
+package tpp.pottable.navtab.DatabaseTab.PlantInfoRecyclerView;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
-public class DatabaseCategoryInside extends AppCompatActivity {
+import tpp.pottable.R;
+
+public class PlantInfoRecyclerView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
